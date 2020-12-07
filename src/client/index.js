ac587bec551830b929339bc681a730ca4e0ffc81
ahmedallani <ahmed.allani@rbk.tn>
2020-12-07 15:59:18 +0100

    Stage 3

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,6 @@
-import "./styles/main.scss";
+import "./styles/app.scss";
 import "./styles/modal.scss";
+import "./styles/form.scss";
 import "./styles/analysis.scss";
 import { App } from "./js/App";
 
