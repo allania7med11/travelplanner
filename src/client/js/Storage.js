e407629d37bde85f70e60485a67c8a5eb966c339
ahmedallani <allania7med1@gmail.com>
2020-12-27 18:11:25 +0100

    state

--- src/client/js/Storage.js
+++ src/client/js/Storage.js
@@ -1,14 +1,39 @@
+let storageTest = require("Data/storage.json");
+console.log({ storageTest });
 class Storage {
   constructor() {
     let local = window.localStorage.getItem("trips");
-    if (local === undefined) {
-      local = JSON.stringify({
-        id: 0,
-        data: {},
-      });
+    if (local === null) {
+      local = JSON.stringify(storageTest);
       window.localStorage.setItem("trips", local);
     }
     this.storage = JSON.parse(local);
+  }
+  state({ start_date, end_date }) {
+    debugger
+    let today = new Date();
+    let start = new Date(start_date);
+    let end = new Date(end_date);
+    let cvt = 1000 * 3600 * 24;
+    if (start.getTime() - today.getTime() > 0) {
+      let days = Math.ceil((start.getTime() - today.getTime()) / cvt);
+      if(days === 1){
+        return "one day to start"
+      }
+      return `${days} days to start`;
+    } else if (end.getTime() - today.getTime() > 0) {
+      let days = Math.ceil((today.getTime() - start.getTime()) / cvt);
+      if(days === 1){
+        return "started one day ago"
+      }
+      return `started ${days} days ago`;
+    } else {
+      let days = Math.ceil((end.getTime() - today.getTime()) / cvt);
+      if(days === 1){
+        return "finished one day ago"
+      }
+      return `finished ${days} days ago`;
+    }
   }
   create(obj) {
     this.storage.id = this.storage.id + 1;
@@ -20,11 +45,18 @@ class Storage {
     let keys = Object.keys(this.storage.data).sort(
       (ky1, ky2) => parseInt(ky2) - parseInt(ky1)
     );
-    let lst=keys.map(cv => this.storage.data[cv])
-    return lst
+    let lst = keys.map((key) => ({
+      state: this.state(this.storage.data[key]),
+      ...this.storage.data[key],
+    }));
+    return lst;
   }
-  get(id){
-      return this.storage.data[id]
+  get(id) {
+    let trip = this.storage.data[id];
+    return {
+      state: this.state(trip),
+      ...trip,
+    };
   }
   update(id, obj) {
     this.storage.data[id] = obj;
--- src/client/js/Table.js
+++ src/client/js/Table.js
@@ -1,11 +1,25 @@
 class Table {
     constructor(app){
         this.app = app
+        this.data = []
+        this.$tbody = document.getElementById("tbody");
+        this.render()
     }
     html() {
-        return /* html */ `
-        
-        `
+        let trips = this.data.map(trip=> /* html */`
+        <tr>
+            <td>${trip.place}</td>
+            <td>${trip.state}</td>
+            <td>${trip.place}</td>
+        </tr>
+        `)
+        return  trips.join('')
+    }
+    render(){
+        this.$tbody.innerHTML = "";
+        this.data = this.app.storage.getall()
+        console.log("test",this.data)
+        this.$tbody.innerHTML = this.html();
     }
 }
 module.exports = { Table };
